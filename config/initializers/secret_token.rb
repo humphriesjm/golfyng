f8d588276faf5895e015b75f91091935482ff4be
Jason Humphries <humphriesj@gmail.com>
2013-06-03 18:06:30 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Golfying::Application.config.secret_token = 'e65950098d2326a2f408ff41aa30ccefab34cca6b8ca887d317d579a74140e758e62a1d7dd6c31ac042ce9c637908a0ecb021fc3fbabe43ef6c5288512f89f89'
