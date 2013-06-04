e827b4791095098115352feff7253b4c09be5ebc
Jason Humphries <humphriesj@gmail.com>
2013-06-03 22:30:01 -0400

    nokotest

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'rails', '3.2.12'
 
 gem 'pg'
 gem 'heroku'
+gem 'nokogiri'
+gem 'sqlite3'
 
 # Gems used only for assets and not required
 # in production environments by default.
---
+++ script/nokotest.rb
@@ -0,0 +1,42 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
+url = "http://www.pga.com/golf-instruction/instructors/"
+doc = Nokogiri::HTML(open(url))
+# country level
+doc.css("#content-area a").each do |item|
+	url2 = item['href']
+	doc2 = Nokogiri::HTML(open("http://www.pga.com#{url2}"))
+	# state level
+	doc2.css("#content-area a").each do |item2|
+		url3 = item2['href']
+		doc3 = Nokogiri::HTML(open("http://www.pga.com#{url3}"))
+		# page of pros - region level
+		doc3.css(".views-row").each do |item3|
+			doc4 = Nokogiri::HTML(item3.text)
+			url4 = item3.at_css(".imagecache-pgapro_medium")['href']
+			if !url4.include? 'http'
+				url4 = "http://www.pga.com" << url4
+			end
+			doc5 = Nokogiri::HTML(open(url4))
+			# single pro page
+			name = doc5.at_css('.title').text
+			phone = doc5.at_css('.contact-field').text
+			if phone.include? 'Phone'
+				phone = phone[5,phone.length]
+			end
+			course_address = doc5.at_css('.pga-professional-course:nth-child(3)').text
+			course_name = doc5.at_css('.field-field-course-name:nth-child(1) a').text
+			description = doc5.at_css('.field-wrap').text
+			image = doc5.at_css('.imagecache')['src']
+			puts "Name: #{name}"
+			puts "Description: #{description}"
+			puts "Course Name: #{course_name}"
+			puts "Course Address: #{course_address}"
+			puts "Phone: #{phone}"
+			puts "Image: #{image}"
+			puts "---------------------"
+		end
+	end
+end
